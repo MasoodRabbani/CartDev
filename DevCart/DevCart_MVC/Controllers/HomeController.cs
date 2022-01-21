46d96be970eac5dd039d323fab4112f2c0332827
Masood <mr.rabbani1380@gmail.com>
2022-01-21 23:22:19 +0330

    contact form clint side validation implementes

--- DevCart/DevCart_MVC/Controllers/HomeController.cs
+++ DevCart/DevCart_MVC/Controllers/HomeController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DevCart_MVC.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevCart_MVC.Controllers
 {
@@ -16,9 +19,32 @@ namespace DevCart_MVC.Controllers
             return View();
         }
 
+        public FileResult DM()
+        {
+            return File("~/test.txt","text/html");
+        }
+
+        public JsonResult Test()
+        {
+            var model = new {name = "masood", family = "rabbani", phonenumber = "09016319519"};
+            return Json(model);
+        }
+
         public IActionResult Contact()
         {
+            //Dictionary<int, string> model = new Dictionary<int, string>()
+            //{
+            //    {1, "الماس"},
+            //    {2, "پلاتین"},
+            //    {3, "نقره ای"}
+            //};//{1,"نقره ای ",2, "پلاتین",3, "الماس",4, "الماس"};
+            //ViewBag.listmodel = new SelectList(model,model.Keys);
             return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DevCart/DevCart_MVC/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCart_MVC.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "این فیلد نمیتواند خالی باشد.")]
+        [MaxLength(10,ErrorMessage = "حداقل  100 کاراکتر باشد")]
+        [MinLength(3,ErrorMessage = "حداکثر 3 کاراکتر باید باشد.")]
+        [Display(Name = "نام")]
+        public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "ایمیل صحیح نیست.!")]
+        [Required(ErrorMessage = "این فیلد نمیتواند خالی باشد.")]
+        public string Email { get; set; }
+        public string Services { get; set; }
+        public string Text { get; set; }
+    }
+}
